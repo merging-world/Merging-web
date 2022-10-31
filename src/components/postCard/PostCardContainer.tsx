a1b764ce2fc84fd74860bead2952802428b69219
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-10-31 14:48:55 +0900

    :construction: wip

--- src/components/postCard/PostCardContainer.tsx
+++ src/components/postCard/PostCardContainer.tsx
@@ -1,10 +1,22 @@
 import styled from '@emotion/styled';
+import PostCardHeader from 'components/postCard/PostCardHeader';
 
 const PostCardContainer = () => {
-  return <Container>PostCardContainer</Container>;
+  return (
+    <Container>
+      <PostCardHeader />
+      <article>
+        게시글 내용
+        <ul>테그</ul>
+      </article>
+      <article>이미지</article>
+      <ul>좋아요 및 북마크</ul>
+    </Container>
+  );
 };
 
 const Container = styled.section`
+  padding: 16px;
   border-radius: 20px;
   background-color: ${props => props.theme.colors.BACKGROUND_SECONDARY};
   background-color: #3ddf3d;
---
+++ src/components/postCard/PostCardHeader.tsx
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled';
+import Profile from 'components/layouts/Profile';
+
+const PostCardHeader = () => {
+  return (
+    <Wrap>
+      <ProfileWrap>
+        <li>
+          <Profile info="profile" url="https://github.com/Park-Wonbin.png" />
+        </li>
+        <ProfileTextWrap>
+          <Nickname>닉네임</Nickname>
+          <TimeLog>10분전</TimeLog>
+        </ProfileTextWrap>
+      </ProfileWrap>
+      <li>3개점</li>
+    </Wrap>
+  );
+};
+
+const Wrap = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 38px;
+`;
+
+const ProfileWrap = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const ProfileTextWrap = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin-left: 8px;
+`;
+
+const Nickname = styled.li`
+  font-size: ${props => props.theme.fontSize.FONT_BODY2};
+  color: ${props => props.theme.colors.TEXT_HIGH};
+`;
+
+const TimeLog = styled.li`
+  font-size: ${props => props.theme.fontSize.FONT_BODY3};
+  color: ${props => props.theme.colors.TEXT_MEDIUM};
+`;
+
+export default PostCardHeader;
